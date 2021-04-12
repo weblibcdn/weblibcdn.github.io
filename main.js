67e7a3a24fbdce0c6794f8ad9f0c5cbfc434cff6
Ethan <65051086+waitblock@users.noreply.github.com>
2021-04-12 17:47:48 -0400

    Update

--- main.js
+++ main.js
@@ -12,3 +12,18 @@ function copy() {
   /* Alert the copied text */
   alert("Copied!");
 }
+
+function minCopy() {
+  /* Get the text field */
+  var copyText = document.getElementById("copyMinText");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+  /* Copy the text inside the text field */
+  document.execCommand("copy");
+
+  /* Alert the copied text */
+  alert("Copied!");
+}
